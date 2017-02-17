0f964081bb3ae7f428d7994f7bdf5bb4c03a88e0
Michele Finotto <m@finotto.org>
2017-02-17 14:52:07 +0100

    Added caching via etags

--- lib/microservice_dsl.rb
+++ lib/microservice_dsl.rb
@@ -1,5 +1,8 @@
 require "microservice_dsl/version"
 require "typhoeus"
+require "json"
+require "digest"
+require "redis"
 
 module Kernel
   private
@@ -10,7 +13,29 @@ module Kernel
     url = ENV["#{microservice.upcase}_URL"] || "http://#{microservice}.#{ENV['MS_DOMAIN'] || Rails.env}:#{ENV["MS_#{microservice.upcase}_PORT"] || '3000'}"
     url << "/#{args[:path]}" if args[:path]
     body = (args[:body].is_a? String) ? args[:body] : args[:body].to_json if args[:body]
-    Typhoeus::Request.new(url, method: args[:method] || :get, headers: MicroserviceDSL.default_headers.merge(args[:headers] || {}).merge({MicroserviceDSL.hop_header => MicroserviceDSL.hop_string}), body: body, params: args[:params], timeout: ENV['MS_DEFAULT_TIMEOUT'] || 10)
+    headers = MicroserviceDSL.default_headers.merge(args[:headers] || {}).merge({MicroserviceDSL.hop_header => MicroserviceDSL.hop_string})
+    method = args[:method] || :get
+    req = Typhoeus::Request.new(url, method: method, headers: headers, body: body, params: args[:params], timeout: ENV['MS_DEFAULT_TIMEOUT'] || 10)
+    
+    if method.to_s.downcase == "get"
+      req.on_complete do |res|
+        if res.code == 200 && etag = res.headers["etag"]
+          MicroserviceDSL.set_cache(url, etag, res)
+        end
+      end
+      if cached = MicroserviceDSL.get_cache(url, req)
+        req.options[:headers]["If-None-Match"] = cached[:etag]
+        req.on_complete do |res|
+          if res.code == 304
+            res.options[:response_body] = cached[:response][:body]
+            res.options[:response_code] = cached[:response][:code]
+            res.options[:headers] = Typhoeus::Response::Header.new(cached[:response][:headers])
+          end
+        end
+      end
+      
+    end
+    req
   end
 
   def call_microservice(microservice, args={})
@@ -21,6 +46,83 @@ module Kernel
 end
 
 module MicroserviceDSL
+  def self.redis_reset!
+    @@redis=nil
+  end
+  
+  def self.redis
+    return nil unless caching?
+    splitted = ENV["MS_CACHE_URL"].split(":")
+    host = splitted[0]
+    port = splitted[1] || "6379"
+    @@redis ||= Redis.new(host: host, port: port)
+  end
+  
+  def self.caching?
+    if ENV["MS_CACHE_URL"]
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.set_cache(url, etag, res)
+    return nil unless caching?
+    puts "Etag: #{etag}"
+    data = {
+      etag: etag,
+      response: {
+            body: res.body,
+            code: res.code,
+            headers: res.headers
+          }
+    }
+    puts data
+    redis.set([redis_hash_name, cache_key(url, res.request)].join(':'), JSON.dump(data))
+  end
+  
+  def self.cache_key(url, req)
+    @@taggers ||= nil
+    url = url.gsub(":", "|")
+    unless @@taggers
+      url
+    else
+      @@taggers.map {|t| Digest::MD5.hexdigest(req.options[:headers][t])}.join("/") + "-" + url
+    end
+  end
+  
+  def self.get_etag(url, req)
+    return nil unless caching?
+    if val = get_cache(url, req)
+      val[:etag]
+    else
+      nil
+    end
+  end
+  
+  def self.has_cache?(url, etag, req)
+    return nil unless caching?
+    (stored_etag = get_etag(url, req)) && (stored_etag == etag) && stored_etag
+  end
+  
+  def self.get_cache(url, req)
+    return nil unless caching?
+    if val = redis.get([redis_hash_name, cache_key(url, req)].join(':'))
+      JSON.parse(val, symbolize_names: true)
+    else
+      nil
+    end
+  end
+  
+  def self.redis_hash_name
+    ENV["MS_CACHE_HASH"] || "msdsl"
+  end
+  
+  def self.cache_taggers=(taggers)
+    raise unless taggers.is_a?(Array)
+    @@taggers = taggers
+  end
+  
   def self.hop_header
     "X-Hop-Count"
   end
@@ -57,6 +159,17 @@ module MicroserviceDSL
   
   def self.hop_string
     [self.current_hop, self.next_hop].join(".")
+  end
+  
+  def self.hashable_string_for(obj)
+    case obj
+    when Hash
+      hashable_string_for(obj.sort_by {|sub_obj| sub_obj.first.to_s})
+    when Array
+      obj.map {|sub_obj| hashable_string_for(sub_obj)}.to_s
+    else
+      obj.to_s
+    end
   end
 end
 
